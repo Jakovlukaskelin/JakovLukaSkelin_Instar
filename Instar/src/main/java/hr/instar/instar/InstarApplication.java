2d598bae43d25185a46121058aa16837005aecfb
Jakov <jakov.skelin@aether-signum.hr>
2024-06-21 12:47:55 +0200

    v3

--- Instar/src/main/java/hr/instar/instar/InstarApplication.java
+++ Instar/src/main/java/hr/instar/instar/InstarApplication.java
@@ -2,16 +2,12 @@ package hr.instar.instar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class InstarApplication extends SpringBootServletInitializer {
+public class InstarApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(InstarApplication.class);
-    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(InstarApplication.class, args);
